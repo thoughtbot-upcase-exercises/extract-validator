bd7c9c495f2db77df92217c1305a3f1cbf4476b0
WendyBeth <wendybeth010@gmail.com>
2015-02-16 19:04:08 -0500

    extract validator. need to study this more to meet full criteria for exercise.

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,17 @@
-class Person
-  include ActiveModel::Validations
+class PhoneNumberFormat < ActiveModel::Validator
   PHONE_REGEX = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/
 
-  validates :phone_number, format: { with: PHONE_REGEX, multiline: true, message: 'invalid phone number formatting' }
+  def validate(record)
+    unless record.phone_number =~ PHONE_REGEX
+      record.errors[:phone_number] << "invalid phone number format"
+    end
+  end
+end
+
+class Person
+  include ActiveModel::Validations
+
+  validates_with PhoneNumberFormat
 
   attr_accessor :phone_number
 

490d37c5e8d52f99049ca7ec4eabbb801a99711f
WendyBeth <wendybeth010@gmail.com>
2015-02-16 19:21:39 -0500

    added tests

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,9 @@
-class PhoneNumberFormatValidator < ActiveModel::Validator
-  PHONE_REGEX = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/
-
-  def validate(record)
-    unless record.phone_number =~ PHONE_REGEX
-      record.errors[:phone_number] << "invalid phone number format"
-    end
-  end
-end
+require 'phone_number_format_validator'
 
 class Person
   include ActiveModel::Validations
 
-  validates :phone_number, phone_number_format: true
+  validates :phone_number, presence: true, phone_number_format: true
 
   attr_accessor :phone_number
 
---
+++ lib/phone_number_format_validator.rb
@@ -0,0 +1,9 @@
+class PhoneNumberFormatValidator < ActiveModel::Validator
+  PHONE_REGEX = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/
+
+  def validate(record)
+    unless record.phone_number.nil? || record.phone_number =~ PHONE_REGEX
+      record.errors[:phone_number] << "invalid phone number format"
+    end
+  end
+end
